2fd5e0e3298eb27684b6138f3b9e321b32707cba
Jessica <jessikurt@gmail.com>
2020-08-21 13:19:07 +0100

    feat: Created Contact Component

---
+++ src/components/ContactPage/ContactPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const ContactPage = () => {
+  return (
+    <div>
+      <h1>
+        Contato
+      </h1>
+    </div>
+    );
+}
+
+export default ContactPage;
