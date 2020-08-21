8bd855ba615c644f510adba6e61454ed9e55bcac
Jessica <jessikurt@gmail.com>
2020-08-21 13:30:18 +0100

    feat: Add HomePages on App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import HomePages from './pages/HomePages';
+
 function App() {
   return (
     <div className="App">
-      <h1> Hello World! </h1>
+      <HomePages />
     </div>
   );
 }
