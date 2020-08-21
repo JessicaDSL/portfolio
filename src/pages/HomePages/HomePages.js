eea0972353ae6fbf9afae9919dc9d0e18d4e8079
Jessica <jessikurt@gmail.com>
2020-08-21 13:31:09 +0100

    feat: Created HomePages

---
+++ src/pages/HomePages/HomePages.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// import { Container } from './styles';
+import HomePage from '../../components/HomePage';
+
+const HomePages = () => {
+  return (
+    <div>
+      <HomePages />
+    </div>
+    );
+}
+
+export default HomePage;
