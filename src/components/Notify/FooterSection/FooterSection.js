0b073e481c14d024a34514e7170b594cf8e96109
Jessica <jessikurt@gmail.com>
2020-08-22 11:37:14 +0100

    feat: Add FooterSection on NotifyPage

---
+++ src/components/Notify/FooterSection/FooterSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const FooterSection = () => {
+  return(
+    <Container>
+      <p>Contact</p>
+      <p>Download</p>
+      <p>Press</p>
+      <p>Email</p>
+      <p>Support</p>
+      <p>Privacy Policy</p>
+    </Container>
+  );
+}
+
+export default FooterSection;
---
+++ src/components/Notify/FooterSection/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #e3e3e3;
+  height: 130px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  p {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    font-size: 16px;
+    color: #848484;
+    cursor: pointer;
+    margin: 0 20px 0 0;
+  }
+`;
