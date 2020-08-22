f710c5aad363e23d06b3d05f2c4fcf131fe36ab1
Jessica <jessikurt@gmail.com>
2020-08-22 12:47:42 +0100

    feat: Included component section for NotifyPage

--- src/pages/NotifyPage/NotifyPage.js
+++ src/pages/NotifyPage/NotifyPage.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
 // import { Container } from './styles';
-import HeroSection from '../../components/Notify/HeroSection';
+import HeroSection from "../../components/Notify/HeroSection";
+import FeatureSection from "../../components/Notify/FeatureSection";
+import SubscribeSection from "../../components/Notify/SubscribeSection";
+import SponserSection from "../../components/Notify/SponserSection";
+import FooterSection from "../../components/Notify/FooterSection";
 
 function NotifyPage() {
   return (
-    <HeroSection />
+    <>
+      <HeroSection />
+      <FeatureSection />
+      <SubscribeSection />
+      <SponserSection />
+      <FooterSection />
+    </>
   );
 }
 
