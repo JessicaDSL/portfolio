a9bf76864066f8e471408487b9ce2c025bff0632
Jessica <jessikurt@gmail.com>
2020-08-28 10:08:30 +0100

    refactor: Cleaning the code to be more readable

--- src/components/Notify/FeatureSection/FeatureSection.js
+++ src/components/Notify/FeatureSection/FeatureSection.js
@@ -1,56 +1,39 @@
 import React from "react";
 
-import { Container, List, ListItem } from "./styles";
+import { Container, Content } from "./styles";
 import Settings from "../../../assets/image/notify/setting.png";
 import Star from "../../../assets/image/notify/star.png";
 import World from "../../../assets/image/notify/world.png";
 
-const list = [
-  {
-    image: Settings,
-    title: "Editable Theme",
-    descripition:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.",
-  },
-  {
-    image: Star,
-    title: "Flat Design",
-    descripition:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.",
-  },
-  {
-    image: World,
-    title: "Reach Your Audience ",
-    descripition:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.",
-  },
-];
-
-const FeatureItem = ({ image, title, descripition }) => {
-  return (
-    <List>
-      <ListItem>
-        <img src={image} alt="icone" />
-      </ListItem>
-      <ListItem>
-        <strong>{title}</strong>
-        <p>{descripition}</p>
-      </ListItem>
-    </List>
-  );
-};
-
 const FeatureSection = () => {
   return (
     <Container>
-      {list.map((item, index) => (
-        <FeatureItem
-          key={index}
-          image={item.image}
-          title={item.title}
-          descripition={item.descripition}
-        />
-      ))}
+      <Content>
+        <img src={Settings} alt="icone" />
+        <strong>Editable Theme</strong>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          fringilla fringilla.
+        </p>
+      </Content>
+
+      <Content>
+        <img src={Star} alt="icone" />
+        <strong>Flat Design</strong>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          fringilla fringilla.
+        </p>
+      </Content>
+
+      <Content>
+        <img src={World} alt="icone" />
+        <strong>Reach Your Audience</strong>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+          fringilla fringilla.
+        </p>
+      </Content>
     </Container>
   );
 };
--- src/components/Notify/FeatureSection/styles.js
+++ src/components/Notify/FeatureSection/styles.js
@@ -4,14 +4,27 @@ import media from "styled-media-query";
 export const Container = styled.div`
   min-height: 335px;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
+  display: flex;
+
+  ${media.lessThan("medium")`
+   flex-wrap: wrap;
+  `}
+`;
+
+export const Content = styled.div`
+  text-align: center;
+  max-width: 350px;
+  display: flex;
+  flex-direction: column;
+  margin: 10px 30px;
 
   strong {
     color: #222222;
     font-family: Raleway;
     font-size: medium;
-    margin: 20px 0;
+    margin: 20px 0 0 0;
     font-size: large;
   }
 
@@ -21,23 +34,12 @@ export const Container = styled.div`
     font-size: 16px;
     line-height: 24px;
     font-weight: lighter;
+    max-width: 400px;
   }
-
-  ${media.lessThan("medium")`
-   flex-wrap: wrap;
-  `}
-
-  ${media.between("medium", "large")`h2{
-      min-width: 100px;
-    }
-  `}
-`;
-
-export const List = styled.ul`
-  padding: 0;
-`;
-
-export const ListItem = styled.li`
-  max-width: 330px;
-  text-align: center;
+  img {
+    width: 110px;
+    height: 110px;
+    margin: 0 auto;
+    padding: 0 0 10px 0;
+  }
 `;
