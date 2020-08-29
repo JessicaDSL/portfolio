96c9e73620043896add7407e898aa4f4dea6afc1
Jessica <jessikurt@gmail.com>
2020-08-29 10:37:14 +0100

    fix: Fixed media query in all sizes in the Hero Section

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -11,50 +11,81 @@ export const Nav = styled.nav`
   align-items: center;
   h1 {
     margin: 0 0 0 8px;
-    color: #f1f1f1;
+    color: #f5f5f5;
     font-family: Lato;
   }
+  ${media.lessThan("321px")`
+    height: 45px;
+    h1 {
+      margin: 0 0 0 4px;
+    }
+  `}
+
   ${media.lessThan("1024px")`
   background: #111111;
-  box-shadow: 0 0 15px rgba(0, 0, 0, 0.4);
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.6);
   `};
 `;
 
 export const Logo = styled.div`
-  margin: 0 0 0 70px;
   display: flex;
-
+  align-items: center;
+  margin: 0 0 0 20px;
   img {
-    width: 45px;
-    height: 35px;
+    width: 55px;
+    height: 45px;
   }
-
-  ${media.lessThan("1024px")`
-      margin: 0 0 0 20px;
+  ${media.lessThan("769px")`
+  margin: 0 0 0 10px;
+  img {
+    height: 35px;
+    width: 45px;
+  }
+  `}
+  ${media.between("769px","1024px" )`
+  img {
+    height: 35px;
+    width: 45px;
+  }
   `};
+  ${media.between("1024px","2000px" )`
+  img {
+    height: 40px;
+    width: 50px;
+  }
+  `}
+  ${media.greaterThan("2000px")`
+  img {
+    height: 45px;
+    width: 55px;
+  }
+  `}
+
+  
 `;
 
-export const Ul = styled.ul`
+export const List = styled.ul`
   display: flex;
   justify-content: space-around;
+  align-items: center;
   color: white;
-  padding: 0 60px 0 0;
+  z-index: 2;
   h1 {
     font-size: 14px;
-    padding: 16px 22px;
-    letter-spacing: 3px;
+    padding: 16px 12px;
+    letter-spacing: 2px;
     background: none;
     border: 0;
     color: #f5f5f5;
     transition: 0.2s all ease-in;
-
+    font-family: Lato;
     &:hover {
       border-bottom: 6px solid #de3163;
       color: #de3163;
     }
   }
-
-  ${media.lessThan("1024px")`
+  
+  ${media.lessThan("769px")`
   flex-flow: column nowrap;
   align-items: center;
   background-color: rgba(0, 0, 0, 0.9);
@@ -67,6 +98,13 @@ export const Ul = styled.ul`
   padding-top: 3.4rem;
   transition: transform 0.3s ease-in-out;
   `}
+  ${media.greaterThan("770px" )`
+  margin: 0 15px 0 0;
+  h1 {
+    font-size: 14px;
+  }
+      
+  `};
 `;
 
 export const StyledBurguer = styled.div`
@@ -78,12 +116,24 @@ export const StyledBurguer = styled.div`
   z-index: 20;
   display: none;
 
-  ${media.lessThan("768px")`
+
+  ${media.lessThan("326px")`
   display: flex;
   justify-content: space-around;
   flex-flow: column nowrap;
+  top: 8px;
+  right: 11px;
+  `}
+  ${media.between("326px","769px")`
+  display: flex;
+  justify-content: space-around;
+  flex-flow: column nowrap;
+  top: 10px;
+  right: 11px;
   `};
-
+  ${media.greaterThan("769px")`
+    display: none;
+  `}
   div {
     width: 2rem;
     height: 0.25rem;
