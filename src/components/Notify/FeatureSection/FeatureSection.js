79df438add9724e018625e3a141d30f35a6e8655
Jessica <jessikurt@gmail.com>
2020-08-30 11:04:13 +0100

    refactor: Clean code for more readable on Feature Section

--- src/components/Notify/FeatureSection/FeatureSection.js
+++ src/components/Notify/FeatureSection/FeatureSection.js
@@ -1,22 +1,20 @@
 import React from "react";
 
 import { Container, List, ListItem } from "./styles";
-import { list } from './content';
+import { list } from "./content";
 
 const FeatureSection = () => {
   return (
     <Container>
-      
       <List>
-        {list.map((item, index) =>
+        {list.map((item, index) => (
           <ListItem key={index}>
-             <img src={item.image} alt="icone"/>
-             <b>{item.title}</b>
-             <p>{item.descripition}</p>
+            <img src={item.image} alt="icone" />
+            <b>{item.title}</b>
+            <p>{item.descripition}</p>
           </ListItem>
-        )}
+        ))}
       </List>
-
     </Container>
   );
 };
--- src/components/Notify/FeatureSection/content.js
+++ src/components/Notify/FeatureSection/content.js
@@ -1,26 +1,24 @@
-
-import Settings from '../../../assets/image/notify/setting.png';
-import Star from '../../../assets/image/notify/star.png';
-import World from '../../../assets/image/notify/world.png';
-
+import Settings from "../../../assets/image/notify/setting.png";
+import Star from "../../../assets/image/notify/star.png";
+import World from "../../../assets/image/notify/world.png";
 
 export const list = [
-{
-  image: Settings,
-  title: "Editable Theme",
-  descripition:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.",
-},
-{
-  image: Star,
-  title: "Flat Design",
-  descripition:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.",
-},
-{
-  image: World,
-  title: "Reach Your Audience ",
-  descripition:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.",
-},
+  {
+    image: Settings,
+    title: "Editable Theme",
+    descripition:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.",
+  },
+  {
+    image: Star,
+    title: "Flat Design",
+    descripition:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.",
+  },
+  {
+    image: World,
+    title: "Reach Your Audience ",
+    descripition:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse fringilla fringilla.",
+  },
 ];
