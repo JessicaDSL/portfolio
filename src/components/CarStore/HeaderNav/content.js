bcebcff39004193e8cb965e0a45bc7d40816f720
Jessica <jessikurt@gmail.com>
2020-09-02 11:44:43 +0100

    feat: Created HeaderNav

---
+++ src/components/CarStore/HeaderNav/content.js
@@ -0,0 +1,20 @@
+export const links = [
+  {
+    label: "Home", url: '#'
+  },
+  {
+    label: "Services", url: '#'
+  },
+  {
+    label: "About", url: '#'
+  },
+  {
+    label: "Reviews", url: '#'
+  },
+  {
+    label: "Locations", url: '#'
+  },
+  {
+    label: "Contacts", url: '#'
+  }
+]
---
+++ src/components/CarStore/HeaderNav/styles.js
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Container = styled.div``;
+
+export const Nav = styled.nav`
+  background-color: #030303;
+  min-height: 55px;
+  font-family: Lato;
+  font-weight: bolder;
+  text-transform: uppercase;
+  font-size: smaller;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  a {
+    color: #ffffff;
+    margin: 0 0 0 40px;
+  }
+`;
+
+export const List = styled.ul`
+  display: flex;
+`;
+
+export const ListItem = styled.li`
+`;
