6b96b6ba198b57d24e69bfabd5be83da8e03fa35
Jessica <jessikurt@gmail.com>
2020-09-04 17:10:13 +0100

    style: Change style on HeaderSearch

--- src/components/CarStore/HeaderSearch/styles.js
+++ src/components/CarStore/HeaderSearch/styles.js
@@ -1,30 +1,52 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 50px;
+  height: 35px;
   color: #ff3448;
   font-family: Lato;
   font-size: small;
   display: flex;
-  flex-direction: row;
   background-color: #212121;
   align-items: center;
   justify-content: flex-end;
+
+  svg{
+    color: #ffffff;
+    margin: 0 5px 0 0;
+  }
+
+  span {
+    
+    svg {
+      color: #ff3448;
+      width: 15px;
+      height: 15px;
+    }
+  }
+
+div {
+  height: 26px;
+  width: 2px;
+  background: #585858;
+  margin: 0 10px 0 0;
+}
 `;
 
 export const List = styled.ul`
-  
+  display: flex;
 `;
 
 export const ListItem = styled.li`
-  
   a {
     color: #ff3448;
+    margin: 0 25px 0 0;
   }
 `;
 
-
-
-export const r = styled.div`
-  
+export const Line = styled.div`
+  width: 90%;
+    height: 3px;
+    background: #595959;
 `;
+
+export const Content = styled.div``
