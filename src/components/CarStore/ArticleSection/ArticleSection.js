b80090ac2a9c1ff78e5a50685bb155e07dfbb89b
Jessica <jessikurt@gmail.com>
2020-09-04 17:01:12 +0100

    refactor: Change code for to be more readble on ArticleSection

--- src/components/CarStore/ArticleSection/ArticleSection.js
+++ src/components/CarStore/ArticleSection/ArticleSection.js
@@ -11,7 +11,9 @@ const ArticleSection = () => {
         <p>This Offer is valid on all our items</p>
         <input type="submit" id="buy" value="Buy Now" />
       </Content>
+
       <hr size="170" width="3" align="left" color="#fff" />
+
       <Content>
         <h1>25% OFF</h1>
         <h2>on order over $999</h2>
