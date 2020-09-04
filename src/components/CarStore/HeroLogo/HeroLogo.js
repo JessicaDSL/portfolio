9939b92686823fda9b3b79f4f858c5390ee1aa01
Jessica <jessikurt@gmail.com>
2020-09-04 17:20:35 +0100

    refactor: Change code for to be more readble on HeroLogo

--- src/components/CarStore/HeroLogo/HeroLogo.js
+++ src/components/CarStore/HeroLogo/HeroLogo.js
@@ -1,29 +1,30 @@
-import React from 'react';
+import React from "react";
+import { FaMobileAlt } from "react-icons/fa";
 
-import { Container, Content, List, ListItem   } from './styles';
+import { Container, Content } from "./styles";
 
 const HeroLogo = () => {
-  return(
+  return (
     <Container>
       <Content>
-      <h1>
-        Car<span>Store</span>
-      </h1>
-      <h4>Best Car Seller</h4>
+        <h1>
+          Car<span>Store</span>
+        </h1>
+
+        <h4>Best Car Seller</h4>
       </Content>
       <Content>
-      <List>
-        <ListItem>
-        <i class="fas fa-mobile-alt"></i>
-        +123 456 7890
-        </ListItem>
-        <ListItem>
-          <p>Mon-Fri: 8:30am-7:30pm; Sat-Sun: 9:30am-4:30pm</p>
-        </ListItem>
-      </List>
+        <b>
+          <span>
+            <FaMobileAlt />
+          </span>
+          +123 456 7890
+        </b>
+
+        <p>Mon-Fri: 8:30am-7:30pm; Sat-Sun: 9:30am-4:30pm</p>
       </Content>
     </Container>
   );
-}
+};
 
 export default HeroLogo;
