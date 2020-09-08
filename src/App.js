46b880dcff3337276a3c29f4f8a4a4e1925c951d
Jessica <jessikurt@gmail.com>
2020-09-08 12:57:55 +0100

    feat: Change routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
-
+import { BrowserRouter } from "react-router-dom";
 
 import Routes from './routes';
 import Global from './styles/global';
+import Hero from './components/Hero';
+import LayoutPage from './pages/LayoutPage';
 
 
 function App() {
   return (
-    <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Hero />
       <Routes />
       <Global />
-    </>
+      <LayoutPage />
+    </BrowserRouter>
   );
 }
 
