ee31ef40ccbee788dd8c7e85c8e1228483fa5b95
Jessica <jessikurt@gmail.com>
2020-09-08 12:58:42 +0100

    feat: Change routes

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import LayoutPage from "./pages/LayoutPage";
 import ContactPage from "./pages/ContactPage";
+import AutiPage from './pages/AutiPage';
 
 export default function Routes() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Route>
+      <>
         <Route path="/" exact component={HomePage} />
         <Route path="/contact-page" component={ContactPage} />
         <Route path="/layout-page" component={LayoutPage} />
-      </Route>
-    </BrowserRouter>
+        <Route path="/auti" component={AutiPage} />
+      </>
   );
 }
