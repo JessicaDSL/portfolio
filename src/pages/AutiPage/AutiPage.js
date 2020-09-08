3b870d0a777bb361b52ac56390c9ef13d2a1df84
Jessica <jessikurt@gmail.com>
2020-09-08 13:08:19 +0100

    feat: Add new Sections

--- src/pages/AutiPage/AutiPage.js
+++ src/pages/AutiPage/AutiPage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-
-import SubscribeSection from '../../components/Auti/SubscribeSection'
+import MenuNav from '../../components/Auti/MenuNav';
+import HeaderSection from '../../components/Auti/HeaderSection';
+import DonationSection from '../../components/Auti/DonationSection';
+import ArticleSection from '../../components/Auti/ArticleSection';
+import SubscribeSection from '../../components/Auti/SubscribeSection';
 import AskSection from '../../components/Auti/AskSection';
 import FooterSection from '../../components/Auti/FooterSection';
 import { Container } from './styles';
@@ -9,6 +12,10 @@ import { Container } from './styles';
 const AutiPage = () => {
   return(
     <Container>
+      <MenuNav />
+      <HeaderSection />
+      <DonationSection />
+      <ArticleSection />
       <SubscribeSection />
       <AskSection />
       <FooterSection />
