35084390c5c78e4d16ce4d8617f2930e2da8579a
Jessica <jessikurt@gmail.com>
2020-09-09 18:07:01 +0100

    Created Home Page Component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import { Container, Content } from './styles';
+
+const Home = () => {
+  return (
+    <Container>
+      <Content>
+        <h3>HELLO WORLD,</h3>
+        <small> I'm</small>
+        <h1>Jéssica Lima</h1>
+        <p>
+          I’m a web developer, and I was born in Brazil, Rio de Janeiro,
+          currently live in Portugal.
+        </p>
+        <p>
+          I am passionate about technology and making dreams come true through
+          the logic of programming.
+        </p>
+      </Content>
+    </Container>
+  );
+};
+
+export default Home;
