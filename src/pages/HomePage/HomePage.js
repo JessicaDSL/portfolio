7d9f141b8412a3ce2ded7cbea3054899b69b34d7
Jessica <jessikurt@gmail.com>
2020-09-09 18:13:26 +0100

    feat: Change the content for a component

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,29 +1,11 @@
 import React from "react";
 
-import { Container, Content} from "./styles";
-import Hero from "../../components/Hero";
+import Home from "../../components/Home";
 
-const HomePage = () => {
-  return (
-    <Container>
-      <Hero />
-      <Content>
-      <div>
-        <h3>HELLO WORLD,</h3>
-        <small> I'm</small>
-        <h1>Jéssica Lima</h1>
-        <p>
-          I’m a web developer, and I was born in Brazil, Rio de Janeiro,
-          currently live in Portugal.
-        </p>
-        <p>
-          I am passionate about technology and making dreams come true through
-          the logic of programming.
-        </p>
-      </div>
-      </Content>
-    </Container>
+const HomePage = () =>{
+  return(
+    <Home />
   );
-};
+}
 
 export default HomePage;
