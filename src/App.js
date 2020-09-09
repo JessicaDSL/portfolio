399b5a2d22c1153c8a01fd3f113113048cc777db
Jessica <jessikurt@gmail.com>
2020-09-09 18:02:28 +0100

    feat: Add Component Hero on App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React from 'react';
 
 
-import Routes from './routes';
 import Global from './styles/global';
-
+import Routes from './routes';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
-      <Routes />
       <Global />
+      <Hero />
+      <Routes />
     </>
   );
 }
