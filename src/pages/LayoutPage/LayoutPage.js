dafe573fd98c06afe3a3d3da9044ef62dac50371
Jessica <jessikurt@gmail.com>
2020-09-10 09:03:14 +0100

    feat: Add new file

--- src/pages/LayoutPage/LayoutPage.js
+++ src/pages/LayoutPage/LayoutPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import Portfolio from "../../components/Layout/Portfolio";
+import Portfolio from "../../components/Layout";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 // import { Container } from './styles';
 
