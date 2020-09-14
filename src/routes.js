55ae92eb843a87daa97a86599de227890aaa602b
Jessica <jessikurt@gmail.com>
2020-09-14 15:24:10 +0100

    feat: Remove Home, Portfolio and Contact from Routes

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,20 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import HomePage from "./pages/HomePage";
-import PortfolioPage from "./pages/PortfolioPage";
-import ContactPage from "./pages/ContactPage";
-import AutiPage from './pages/AutiPage';
+import DefaultPage from './pages/DefaultPage';
+import AutiPage from "./pages/AutiPage";
 import CarStorePage from "./pages/CarStorePage";
 import NotifyPage from "./pages/NotifyPage";
 
 export default function Routes() {
   return (
-    
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/layouts" component={PortfolioPage} />
+        <Route path="/" exact component={DefaultPage} />
         <Route path="/carstore" component={CarStorePage} />
         <Route path="/auti" component={AutiPage} />
         <Route path="/notify" component={NotifyPage} />
       </Switch>
+    </BrowserRouter>
   );
 }
