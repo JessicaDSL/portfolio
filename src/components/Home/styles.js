fe06fe91c71a62efab3069beabd76fb00e11bf30
Jessica <jessikurt@gmail.com>
2020-09-16 09:26:14 +0100

    style: Included Media Query on Home

--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -11,12 +11,16 @@ export const Container = styled.div`
   font-family: FiraCodeRetina;
   h1 {
     color: #de3163;
+    font-size: 2.5rem;
   }
   h3 {
     margin: 0 0 7% 0;
+    font-size: 1.5rem;
+    margin: 0 0 10% 0;
   }
   p{
     max-width: 90%;
+    margin: 7% 0;
   }
   img {
     width: 100%;
@@ -25,12 +29,23 @@ export const Container = styled.div`
       margin: 15% 0;
     `}
   }
+  ${media.lessThan("1080px")`
+  justify-content: center;
+    `}
 `;
 
 export const Content = styled.div`
 display: flex;
 width: 85%;
 ${media.lessThan("1080px")`
+      margin: 15% 0;
+    `}
+${media.lessThan("1080px")`
       flex-direction: column;
+      text-align: center;
+      width: 90%;
+      p{
+        max-width: 100%;
+      }
     `}
 `;
