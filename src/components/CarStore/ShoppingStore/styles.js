d052d059ac35ee551e68fac46369eb9b798431dc
Jessica <jessikurt@gmail.com>
2020-09-18 18:51:22 +0100

    style: Add Media Query on ShoppingStore

--- src/components/CarStore/ShoppingStore/styles.js
+++ src/components/CarStore/ShoppingStore/styles.js
@@ -1,9 +1,14 @@
 import styled from "styled-components";
+import media from 'styled-media-query';
 
 export const Container = styled.div`
+width: 35%;
   b {
     color: #ff3448;
     padding: 40px;
+    ${media.lessThan("500px")`
+    padding: 10%;
+  `}
   }
 
   p {
@@ -17,20 +22,34 @@ export const Container = styled.div`
   a {
     color: white;
   }
+  ${media.lessThan("500px")`
+  min-height: 150px;
+  display: none;
+  background: red;
+  width: 100%;
+  `}
 `;
 
 export const List = styled.ul`
   display: flex;
-  justify-items: center;
+  justify-content: center;
   align-items: center;
+  ${media.lessThan("500px")`
+  `}
 `;
 
 export const ListItem = styled.li`
   height: 70px;
-  width: 100px;
+  width: 30%;
   display: flex;
-  justify-items: center;
   align-items: center;
   border-left: 3px solid #595959;
-  justify-items: center;
+  ${media.lessThan("500px")`
+  background: blue;
+  &:nth-child(1) {
+    border-left: none;
+    
+    }
+  `}
+  
 `;
