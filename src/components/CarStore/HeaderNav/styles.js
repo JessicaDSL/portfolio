6c17f327360b675e03448f3af1ef4ea5e7cfb615
Jessica <jessikurt@gmail.com>
2020-09-18 13:17:04 +0100

    style: Add Media Query on Nav

--- src/components/CarStore/HeaderNav/styles.js
+++ src/components/CarStore/HeaderNav/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import media from "styled-media-query";
 
 export const Container = styled.div``;
 
@@ -12,16 +13,36 @@ export const Nav = styled.nav`
   display: flex;
   align-items: center;
   justify-content: flex-start;
-  
+
   a {
     color: #ffffff;
     margin: 0 0 0 40px;
+    ${media.lessThan("500px")`
+    margin: 5% 0;
+    `}
   }
+
+  ${media.lessThan("500px")`
+    justify-content: flex-start;
+    align-content: center;
+    min-height: 200px;
+  `}
 `;
 
 export const List = styled.ul`
   display: flex;
+  width: 100%;
+  ${media.lessThan("500px")`
+    flex-direction: column;
+    justify-content: center;
+  `}
 `;
 
 export const ListItem = styled.li`
+  ${media.lessThan("500px")`
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    display: flex;
+  `}
 `;
