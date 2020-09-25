a914cc977bc12a1686f08c11544bcf54c52a86d0
Jessica <jessikurt@gmail.com>
2020-09-25 11:03:39 +0100

    style: fix bug size background

--- src/components/ArrowLeft/styles.js
+++ src/components/ArrowLeft/styles.js
@@ -4,19 +4,14 @@ export const Container = styled.div`
   position: fixed;
   top: 25px;
   left: 10px;
-  width: 15%;
-  height: 30px;
   font-family: FiraCodeRetina;
   display: flex;
   justify-content: center;
   align-items: center;
   background: rgba(0, 0, 0, .3);
-  h1{
-    
-  }
   a{
     color: rgba(255, 255, 255, .7);
-    font-size: .7rem;
+    font-size: .5rem;
   }
   svg {
     margin: 6px 6px 0 0;
