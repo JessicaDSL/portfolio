fd153c1286447aa1125722c8c4574532468d9e9a
Jessica <jessikurt@gmail.com>
2020-09-30 14:41:01 +0100

    style: Fixed Media Query on Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,20 @@
 import React from "react";
 
-import { Container, Content } from "./styles";
+import { Container, Content, Image, Main } from "./styles";
 import Background from "../../assets/image/home/bg01.png";
 
 const Home = () => {
   return (
     <Container id="home">
       <Content>
-        <div>
+        <Main>
           <h1>JÉSSICA LIMA</h1>
           <h3>Frontend Developer</h3>
           <p>
             Hello World, I'm a web developer, and i was born in Brazil, Rio de
             Janeiro. Currently live in Portugal. I am passionate about
             technology, games, creativity, and making dreams come true through
-            the logic of programing.
+            the logic of <br/>programming.
           </p>
           <p>
             I am currently focused on website development and maintenance. Using
@@ -22,10 +22,10 @@ const Home = () => {
             technologies
           </p>
           <p>:)</p>
-        </div>
-        <div>
+        </Main>
+        <Image>
           <img src={Background} alt="" />
-        </div>
+        </Image>
       </Content>
     </Container>
   );
--- src/components/Home/styles.js
+++ src/components/Home/styles.js
@@ -1,51 +1,123 @@
 import styled from "styled-components";
 import media from "styled-media-query";
 
-
 export const Container = styled.div`
-  min-height: 600px;
+  min-height: 480px;
   display: flex;
   justify-content: flex-end;
   align-items: center;
   color: #707070;
   font-family: FiraCodeRetina;
   h1 {
-    color: #de3163;
+    color: #ff0099;
     font-size: 2.5rem;
+    padding: 60px 0 0 0;
   }
   h3 {
-    margin: 0 0 7% 0;
+    margin: 0 0 50px 0;
     font-size: 1.5rem;
-    margin: 0 0 10% 0;
   }
-  p{
-    max-width: 90%;
-    margin: 7% 0;
+  p {
+    font-size: 1.5rem;
   }
   img {
     width: 100%;
     height: auto;
-    ${media.lessThan("1010px")`
-      margin: 15% 0;
-    `}
   }
-  ${media.lessThan("1010px")`
+  ${media.lessThan("910px")`
   justify-content: center;
-    `}
+  min-height: 200px;
+  img {
+    display: none;
+  }
+`}
 `;
 
 export const Content = styled.div`
-display: flex;
-width: 90%;
-${media.lessThan("1010px")`
-  margin: 5% 0;
-  `}
-${media.lessThan("1010px")`
-  flex-direction: column;
+  display: flex;
+  width: 90%;
+  ${media.lessThan("910px")`
   text-align: center;
   width: 90%;
-  p{
-    max-width: 100%;
-  }
+  
 `}
+`;
+
+export const Image = styled.div`
+  /*aqui esta perfeito para todos os tamanhos*/
+  width: 45%;
+  height: auto;
+  ${media.lessThan("910px")`
+    display: none;
+  `}
+  ${media.between("910px", "1400px")`
+      width: 50%;
+  `}
+`;
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  width: 55%;
+  p {
+    max-width: 85%;
+    font-size: 0.9rem;
+    margin: 10px 0;
+  }
+  ${media.lessThan("910px")`
+    width: 100%;
+    p{
+      max-width: 100%;
+    }
+    h1 {
+      padding: 0;
+    }
+    h3 {
+      margin: 0 0 25px 0;
+    }
+  `}
+  ${media.between("910px", "1100px")`
+    width: 50%;
+    h1 {
+      padding: 40px 0 0 0;
+      font-size: 2.3rem;
+    }
+    h3 {
+      font-size: 1.2rem;
+      margin: 0 0 20px 0;
+    }
+    p{
+      max-width: 80%;
+      font-size: .6rem;
+    }
+  `}
+  ${media.between("1100px", "1400px")`
+    width: 50%;
+    h1 {
+      padding: 40px 0 0 0;
+      font-size: 3.5rem;
+    }
+    h3 {
+      font-size: 2rem;
+      margin: 0 0 20px 0;
+    }
+    p{
+      max-width: 80%;
+      font-size: .7rem;
+    }
+  `}
+
+  ${media.greaterThan("2101px")`
+    h1 {
+      font-size: 6rem;
+      margin: 60px 0 0 0;
+    }
+    h3 {
+      font-size: 3.5rem;
+    }
+    p {
+      font-size: 1.6rem;
+      padding: 10px 0;
+    }
+  `}
 `;
