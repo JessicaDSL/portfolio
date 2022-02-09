e727771f05c8b228d5785fa08746c14f4925b577
JessicaLima <jessikurt@gmail.com>
2022-02-08 23:37:20 -0300

    feat: create Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import { Container } from './styles';
+
+const Footer = () => {
+  return (
+    <Container>
+      Feito com <FavoriteBorderIcon/> por Jéssica Lima
+    </Container>
+  )
+}
+
+export default Footer
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #141628;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #d1d5dc;
+  font-size: small;
+  svg {
+    color: #fd4370;
+    margin: 0 .5rem;
+  }
+`;
