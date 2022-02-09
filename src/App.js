28afeba919d70e87d0f7a4cc560fe0e78dbc7aba
JessicaLima <jessikurt@gmail.com>
2022-02-08 23:35:09 -0300

    feat: create react app

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Portfolio from "./components/Portfolio";
+import Home from "./pages/Home/Home";
+import Global from "./styles/global";
+
+const App = () => {
+  return (
+    <Router basename={process.env.PUBLIC_URL}>
+      <Global />
+      <ToastContainer autoClose={3000} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
