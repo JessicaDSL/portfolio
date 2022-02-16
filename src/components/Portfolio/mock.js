2d31b545abe0b4725b3e56b51b8b98296bd41810
JessicaLima <jessikurt@gmail.com>
2022-02-16 10:18:35 -0300

    feat: remove landing pages

--- src/components/Portfolio/mock.js
+++ src/components/Portfolio/mock.js
@@ -20,13 +20,4 @@ export const projects = [
     infoproject:
       "O e-commerce funciona como uma loja virtual e representa um canal de venda online para as empresas. Permitindo assim que o lojista poss comercializar os seus produtos por meio de um site exclusivo. Alguns vendedores utilizam o e-commerce como fonte alternativa de vendas, associando-o a loja física.Uma infinidade de mercadorias pode ser vendida por meio dos e-commerces, que abrangem praticamente todo tipo de categoria. Alguns e-commerces brasileiros de destaque são os sites Amaro, Boticário e Reserva.Esse projeto se trata de uma aplicação de carrinho de compras de um ecommerce de tênis, um projeto em React, responsivo e altamente performático. Foi criado para aprender o uso de Context API, para que realizasse as seguintes funcionalidades:Adicionar um novo produto ao carrinho, remover um produto do carrinho, alterar a quantidade de um produto no carrinho, cálculo dos preços sub-total e total do carrinho, exibição de mensagens de erro e sucesso. Tecnologias utilizadas: ReactJS, Yarn, Axios, React-Toastify, Styled-components, Styled-media-query, Context API, layout responsivo.",
   },
-  {
-    id: 3,
-    name: "LandingPages",
-    subtitle: "Projeto focado em trabalhar o consumo de API",
-    url: "https://jessicadsl.github.io/pokeProject/",
-    image: Pokedex,
-    infoproject:
-      "Esse projeto e uma plataforma para listagem e visualização de pokémons foi criado para estudo do consumo de API, passar props para componentes, ReactJS, react-router-dom, material-UI, styled-components, e layout responsivo, adiciona pokémons à lista, possui lista de pokémons favoritos e uma página com mais detalhes sobre o pokémon escolhido ao clicar na imagem. Nota: A listagem dos pokémons foi limitada em 150, pois a partir desse número a api apresenta alguns erros nas informações do pokémon. As características de batalha de cada pokémon na API não estão precisas, o motivo disso é que eu não possui na api a base do calcúlo de cada espécie, como são várias espécies, foi necessário limitar essas informações.",
-  },
 ];
