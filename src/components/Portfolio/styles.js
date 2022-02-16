69e524e33dcfcfb241286f6fe0b8e6d04f5327da
JessicaLima <jessikurt@gmail.com>
2022-02-15 22:28:57 -0300

    styles: fixed media queries

--- src/components/Portfolio/styles.js
+++ src/components/Portfolio/styles.js
@@ -12,7 +12,7 @@ export const Background = styled.div`
   justify-content: center;
   align-items: center;
   ${media.greaterThan("medium")`
-    height: 500px;
+    
   `}
 `;
 
@@ -23,7 +23,7 @@ export const Title = styled.div`
   justify-content: center;
   opacity: 0;
   width: 86%;
-  margin: 4rem 0 1rem 0;
+  margin: 6rem 0 0.3rem 0;
   p {
     width: 100%;
     display: flex;
@@ -36,7 +36,7 @@ export const Title = styled.div`
     color: #fff;
     font-weight: 600;
     margin: 1rem 0 0 0;
-    font-size: large;
+    font-size: x-large;
   }
   hr {
     height: 0.2rem;
@@ -52,16 +52,20 @@ export const Title = styled.div`
     letter-spacing: 0.9px;
     margin: 0 0 0 0.5rem;
   }
-  ${media.greaterThan("medium")`
-    width: 80%;
-    margin: 1.5rem 0 0rem 0;
+  ${media.between("medium", "large")`//1170
+  //background: red;
+  width: 80%;
+    margin: 6rem 0 1rem 0;
     h2 {
       margin: .8rem 0 0 0;
-      font-size: medium;
+      font-size: x-large;
     }
     h3 {
       font-size: medium;
     }
+  `}
+  ${media.greaterThan("large")`
+    width: 80%;
   `}
 `;
 
@@ -79,6 +83,7 @@ export const Container = styled.div`
   ${media.greaterThan("medium")`
     flex-direction: row;
     width: 80%;
+    
     justify-content: flex-start;
     margin: 1.5rem 0 0 0;
   `}
@@ -90,19 +95,18 @@ export const Info = styled.div`
   width: 100%;
   opacity: 0;
   ${media.greaterThan("medium")`
-    width:80%;
   `}
 `;
 
 export const Arrows = styled.div`
-  margin: 0.5rem 0 0 0;
+  margin: 0.8rem 0 0 0;
   display: flex;
   align-items: center;
   svg {
     font-size: large;
     background: #262a43;
-    width: 2rem;
-    height: 2rem;
+    width: 2.5rem;
+    height: 2.5rem;
     border-radius: 4px;
     cursor: pointer;
     &:hover {
@@ -113,29 +117,52 @@ export const Arrows = styled.div`
   strong {
     margin: 0 1rem;
   }
+  ${media.between("medium", "large")`//1170
+    //background: blue;
+    margin: 0.8rem 0 1.2rem 0;
+    svg {
+    font-size: large;
+    width: 2.8rem;
+    height: 2.8rem;
+  }
+  `}
 `;
 
 export const Image = styled.div`
   width: 100%;
-  margin: 2rem 0;
+  margin: 4rem 0;
   display: flex;
   justify-content: center;
   padding: 0 0 0 2rem;
   background-image: url(${Bg});
   background-position: center center;
   background-repeat: no-repeat;
-  background-size: 90% 90%;
+  background-size: 100% 100%;
   img {
-    width: 75%;
+    width: 90%;
     margin: 0 0 0 0rem;
     opacity: 1;
   }
-  ${media.greaterThan("medium")`
-    background-size: 70% 70%;
+  ${media.between("medium", "large")`
+    background-size: 90% 100%;
+    //background: red;
+    a{
+     // background: blue;
+      display: flex;
+      justify-content: flex-end;
+    }
+    //align-items: flex-end;
+    padding: 0;
     margin: 0;
     img {
-      width: 100%;
-      height: 175px;
+     // padding: 0 0 0 2rem;
+      width: 90%;
+      justify-content: flex-end;
+      //height: 175px;
+      //background: gray;
     }
+  `}
+  ${media.greaterThan("medium")`
+    
   `}
 `;
--- src/components/Project/styles.js
+++ src/components/Project/styles.js
@@ -2,20 +2,20 @@ import styled from "styled-components";
 import media from "styled-media-query";
 
 export const Container = styled.div`
-  margin: 0 0 3rem 0;
+  margin: 0 0 5rem 0;
   h2 {
     margin: 2.2rem 0;
-    font-size: x-large;
+    font-size: xx-large;
     color: #fff;
     font-weight: 700;
   }
   p {
-    max-height: 100px;
+    max-height: 150px;
     overflow-y: auto;
     margin: 1rem 0 0 0;
     color: #d1d5dc;
     letter-spacing: 0.1px;
-    font-size: small;
+    font-size: medium;
     &::-webkit-scrollbar {
       display: none;
     }
@@ -43,10 +43,10 @@ export const Container = styled.div`
   span {
     display: flex;
     align-items: center;
-    font-weight: 500;
+    font-weight: 600;
     color: #fff;
     letter-spacing: 0.3px;
-    font-size: small;
+    font-size: medium;
     margin: 0 0 2rem 0;
     hr {
       width: 1rem;
@@ -61,15 +61,15 @@ export const Container = styled.div`
     list-style: disc;
     padding: 0 0 0 1rem;
   }
-  ${media.greaterThan("medium")`
-    h2 {
-      margin: 1rem 0;
-      font-size: x-large;
+  ${media.between("medium", "large")`
+  h2 {
+      margin: 1rem 0 2rem 0;
+      font-size: xx-large;
     }
     p {
-      max-height: 100px;
-      margin: 0;
-      font-size: small;
+      max-height: 150px;
+      margin: 1rem 0 1rem 0;
+      font-size: medium;
     }
     a {
       button {
@@ -80,13 +80,21 @@ export const Container = styled.div`
       }
     }
     span {
-      font-size: .7rem;
-      margin: 0 0 1rem 0;
+      font-size: medium;
+      margin: 0 0 1.8rem 0;
       hr {
         width: .3rem;
         height: 0.2rem;
         margin: 0 .5rem 0 0;
       }
+    }
+  `}
+  ${media.greaterThan("large")`
+  p {
+      max-height: 190px;
+      max-width: 85%;
+      margin: 1rem 0 0rem 0;
+      font-size: large;
     }
   `}
 `;
