bea58502fde3be55869c446a30849801ea0d2025
JessicaLima <jessikurt@gmail.com>
2022-02-17 12:37:11 -0300

    feat: add a toastify

--- src/components/FormRequest/FormRequest.js
+++ src/components/FormRequest/FormRequest.js
@@ -1,9 +1,14 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
 import emailjs from "emailjs-com";
+
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.min.css";
+
 import { Container } from "./styles";
 
 const FormRequest = () => {
   const form = useRef();
+  const [sizeMessage, setSizeMessage] = useState(0);
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -16,24 +21,31 @@ const FormRequest = () => {
       )
       .then(
         (result) => {
-          console.log(result.text);
+          console.log();
+          toast.success(`${result.text}! The message has been sent!`, {
+            theme: "dark",
+          })
         },
         (error) => {
-          console.log(error.text);
+          toast.error(`${error.text}! The message was not sent!`, {
+            theme: "dark",
+          })
         }
       );
+      form.current.reset();
   };
 
   return (
     <Container>
       <form ref={form} onSubmit={sendEmail} method="POST">
-        <p>Seu nome:</p>
+        <p>Your name:</p>
         <input name="name" type="text" />
-        <p>Email:</p>
+        <p>Your Email:</p>
         <input name="email" type="text" />
         <p>Your Message:</p>
-        <textarea name="message" type="text" cols="5" rows="5"></textarea>
-        <button type="submit" className="form__input — button">
+        <textarea name="message" type="text" cols="5" rows="5" onChange={(e) => setSizeMessage(e.target.value)}></textarea>
+        <span>{sizeMessage.length > 0 ? sizeMessage.length : "0"}/300</span>
+        <button type="submit">
           Send Message!
         </button>
       </form>
