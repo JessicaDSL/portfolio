aba153f449a527ee84d2f6406bf7e209b318c2a2
JessicaLima <jessikurt@gmail.com>
2022-03-24 12:13:41 -0300

    style: add scrollbar

--- src/components/Project/styles.js
+++ src/components/Project/styles.js
@@ -11,13 +11,33 @@ export const Container = styled.div`
   }
   p {
     max-height: 150px;
-    overflow-y: auto;
+    overflow-y: scroll;
     margin: 1rem 0 0 0;
     color: #d1d5dc;
     letter-spacing: 0.1px;
     font-size: medium;
+    padding: 0 1.5rem 0 0;
     &::-webkit-scrollbar {
-      display: none;
+      width: 10px;
+      background-color: #141628;
+      border-radius: 10px;
+    }
+    &::-webkit-scrollbar-thumb {
+      border-radius: 10px;
+      background-color: rgb(27, 30, 50);
+      background-image: -webkit-gradient(
+        linear,
+        40% 0%,
+        75% 84%,
+        from(#535d9d),
+        to(#4a528b),
+        color-stop(0.6, #7e8ce7)
+      );
+    }
+    &::-webkit-scrollbar-track {
+      -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
+      background-color: #141628;
+      border-radius: 10px;
     }
   }
   a {
